4b8d67cf8304de14442cc16d4f6e890598d8c771
Daria Chernovol <fabulus.kg.dk@gmail.com>
2020-01-31 11:44:01 +0600

    done with task-1

---
+++ task-1.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+app.get('/:name', (req, res) => {
+    res.send(req.params.name);
+});
+
+app.listen(port, () => {
+    console.log('This is port: ' + port);
+});
